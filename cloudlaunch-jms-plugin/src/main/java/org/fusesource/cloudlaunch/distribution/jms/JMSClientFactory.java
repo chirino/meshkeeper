02ed11384e6583a2f70afc7124c73b18dc9bf982
cmacnaug <colin.macnaughton@gmail.com>
2009-09-01 21:49:47 +0000

    Moving cloudlaunch-jms-plugin to cloudlaunch-eventviajms-plugin, and moving jms provider apis from cloudlaunch-api to cloudlaunch-jms-plugin.
    This removes compile dependency on jms from cloudlaunch-api

---
+++ cloudlaunch-jms-plugin/src/main/java/org/fusesource/cloudlaunch/distribution/jms/JMSClientFactory.java
@@ -0,0 +1,31 @@
+package org.fusesource.cloudlaunch.distribution.jms;
+
+import org.fusesource.cloudlaunch.distribution.AbstractPluginFactory;
+import org.fusesource.cloudlaunch.distribution.FactoryFinder;
+
+/**************************************************************************************
+ * Copyright (C) 2009 Progress Software, Inc. All rights reserved.                    *
+ * http://fusesource.com                                                              *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the AGPL license      *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+
+/**
+ * JMSClientFactory
+ * <p>
+ * Description:
+ * </p>
+ * 
+ * @author cmacnaug
+ * @version 1.0
+ */
+public class JMSClientFactory extends AbstractPluginFactory<JMSProvider> {
+
+    private static final FactoryFinder FACTORY_FINDER = new FactoryFinder("META-INF/services/org/fusesource/cloudlaunch/distribution/jms/client/");
+
+    @Override
+    protected final FactoryFinder getFactoryFinder() {
+        return FACTORY_FINDER;
+    }
+}
---
+++ cloudlaunch-jms-plugin/src/main/java/org/fusesource/cloudlaunch/distribution/jms/JMSServerFactory.java
@@ -0,0 +1,44 @@
+/**************************************************************************************
+ * Copyright (C) 2009 Progress Software, Inc. All rights reserved.                    *
+ * http://fusesource.com                                                              *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the AGPL license      *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package org.fusesource.cloudlaunch.distribution.jms;
+
+import org.fusesource.cloudlaunch.control.ControlService;
+import org.fusesource.cloudlaunch.control.ControlServiceFactory;
+import org.fusesource.cloudlaunch.distribution.FactoryFinder;
+
+/**
+ * JMSServerFactory
+ * <p>
+ * Description:
+ * </p>
+ * 
+ * @author cmacnaug
+ * @version 1.0
+ */
+public class JMSServerFactory extends ControlServiceFactory {
+
+    private static final FactoryFinder FACTORY_FINDER = new FactoryFinder("META-INF/services/org/fusesource/cloudlaunch/distribution/jms/server/");
+
+    public final FactoryFinder getFactoryFinder() {
+        return FACTORY_FINDER;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.fusesource.cloudlaunch.control.ControlServiceFactory#createControlService
+     * (java.lang.String)
+     */
+    @Override
+    public ControlService createPlugin(String providerUri) throws Exception {
+
+        return create(providerUri);
+    }
+
+}
