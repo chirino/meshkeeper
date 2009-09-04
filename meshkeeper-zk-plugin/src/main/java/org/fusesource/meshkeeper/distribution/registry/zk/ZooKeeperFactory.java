e9c2d95914c4d18ae1f857f8596dab00b0060770
cmacnaug <colin.macnaughton@gmail.com>
2009-09-04 20:57:24 +0000

    Renaming cloudlaunch to meshkeeper

---
+++ meshkeeper-zk-plugin/src/main/java/org/fusesource/meshkeeper/distribution/registry/zk/ZooKeeperFactory.java
@@ -0,0 +1,42 @@
+/**************************************************************************************
+ * Copyright (C) 2009 Progress Software, Inc. All rights reserved.                    *
+ * http://fusesource.com                                                              *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the AGPL license      *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package org.fusesource.meshkeeper.distribution.registry.zk;
+
+import org.fusesource.meshkeeper.distribution.registry.Registry;
+import org.fusesource.meshkeeper.distribution.registry.RegistryFactory;
+import org.fusesource.meshkeeper.util.internal.IntrospectionSupport;
+import org.fusesource.meshkeeper.util.internal.URISupport;
+
+import java.net.URI;
+import java.util.Map;
+
+/**
+ * @author chirino
+ */
+public class ZooKeeperFactory extends RegistryFactory {
+
+    @Override
+    public Registry createPlugin(String uri) throws Exception {
+        URI connectUri = new URI(URISupport.stripPrefix(uri, "zk:"));
+
+        ZooKeeperRegistry registry = new ZooKeeperRegistry();
+
+        //Use query params to initialize the factory:
+        Map<String, String> props = URISupport.parseParamters(connectUri);
+        if (!props.isEmpty()) {
+            IntrospectionSupport.setProperties(registry, URISupport.parseQuery(uri));
+            connectUri = URISupport.removeQuery(new URI(uri));
+            connectUri = URISupport.createRemainingURI(connectUri, props);
+        }
+        registry.setConnectUrl(connectUri.toString());
+        registry.start();
+        return registry;
+
+    }
+
+}
