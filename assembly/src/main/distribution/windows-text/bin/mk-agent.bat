64e3bc4d5d5e37699e26839226498ac7b6315581
Hiram Chirino <hiram@hiramchirino.com>
2009-09-10 19:55:47 +0000

    consolidate the .bat scripts a bit.

--- assembly/src/main/distribution/windows-text/bin/mk-agent.bat
+++ assembly/src/main/distribution/windows-text/bin/mk-agent.bat
@@ -1,4 +1,4 @@
-echo off
+@echo off
 REM ######################################################################################
 REM # Copyright (C) 2009 Progress Software, Inc. All rights reserved.                    #
 REM # http://fusesource.com                                                              #
@@ -6,75 +6,100 @@ REM # --------------------------------------------------------------------------
 REM # The software in this package is published under the terms of the AGPL license      #
 REM # a copy of which has been included with this distribution in the license.txt file.  #
 REM ######################################################################################
+
 setlocal
-TITLE MeshKeeper Agent
+TITLE MeshKeeper Launch Agent
 
 REM =====================================================================
 REM User customization area.  You can set your defaults here.
 REM =====================================================================
 
-REM Set this to a particular JAVA_EXE if you wish
 set JAVA_EXE=
-
-JAVA_MIN_MEM=16m
-JAVA_MAX_MEM=32m
-
-goto BEGIN
-
-REM =====================================================================
-REM This section holds MeshKeeper environment setup routines
-REM =====================================================================
-
-REM =====================
-REM When an error happens
-REM =====================
-:ERROR
-  echo Paused to catch any errors. Press any key to continue.
-  errorlvl 1    
-  pause
-goto :END
-
-REM ===================
-REM Set MESHKEEPER_HOME
-REM ===================
-
-cd %~dp0%
-cd ..
-set MESHKEEPER_HOME=%cd%
-if not exist "%MESHKEEPER_HOME%" (
-    echo MESHKEEPER_HOME directory is not valid: %MESHKEEPER_HOME%
-    goto :ERROR
-)
-
-REM ==============
-REM Setup JAVA_EXE
-REM ==============
-if not "%JAVA_EXE%" == "" goto :CHECK_JAVA_END
-	set JAVA_EXE=%JAVA%
-	if not "%JAVA_EXE%" == "" goto :CHECK_JAVA_END
-    	set JAVA_EXE=java
-:CHECK_JAVA_END
-
-REM ===============
-REM Setup CLASSPATH
-REM ===============
-CLASSPATH=%MESHKEEPER_HOME%\repository\org\fusesource\meshkeeper\meshkeeper-api\${project.version}\meshkeeper-api-${project.version}.jar
-CLASSPATH=%CLASSPATH%;%MESHKEEPER_HOME%\repository\org\fusesource\mop\mop-core\${mop-version}\mop-core-${mop-version}.jar
-
-REM ==========
-REM Setup OPTS
-REM ==========
-set OPTS=-Dlog4j.configuration=file:%MESHKEEPER_HOME%\etc\log4j.properties
-set OPTS=%OPTS% -Dmop.base=$MESHKEEPER_HOME
-set OPTS=%OPTS% -Dmop.online=false
+set JAVA_MIN_MEM=16m
+set JAVA_MAX_MEM=32m
+set MESHKEEPER_OPTS=
 
 REM =====================================================================
 REM Execute the Launch Agent
 REM =====================================================================
 
-cd %MESHKEEPER_HOME%
-%JAVA_EXE% %OPTS% -classpath %CLASSPATH% org.fusesource.meshkeeper.launcher.Main --directory "%MESHKEEPER_HOME%\data" %*
-if ERRORLEVEL 1 GOTO ERROR
+call :SETUP_DEFAULTS
+
+%JAVA_EXE% %JAVA_OPTS% %OPTS% -classpath %CLASSPATH% org.fusesource.meshkeeper.launcher.Main --directory "%MESHKEEPER_HOME%\data" %*
+
+if ERRORLEVEL 1 goto ERROR
+goto END
+
+REM =====================================================================
+REM Environment setup helper routines
+REM =====================================================================
+
+:SETUP_DEFAULTS
+  call :LOCATE_MESHKEEPER_HOME
+  call :LOCATE_MESHKEEPER_BASE
+  call :LOCATE_JAVA_EXE
+  call :LOCATE_JAVA_OPTS
+  call :LOCATE_OPTS
+  call :LOCATE_CLASSPATH
+goto :EOF
+
+:LOCATE_MESHKEEPER_HOME
+  cd %~dp0%
+  cd ..
+  set MESHKEEPER_HOME=%cd%
+  if not exist "%MESHKEEPER_HOME%" (
+    echo MESHKEEPER_HOME directory is not valid: %MESHKEEPER_HOME%
+    goto :ERROR
+  )
+goto :EOF
+
+:LOCATE_MESHKEEPER_BASE
+  if "%MESHKEEPER_BASE%" == "" (
+    set MESHKEEPER_BASE=%MESHKEEPER_HOME%
+  )
+goto :EOF
+
+:LOCATE_JAVA_EXE
+  if "%JAVA_EXE%" == "" (
+    set JAVA_EXE=%JAVA%
+  )
+	if "%JAVA_EXE%" == "" (
+    set JAVA_EXE=java
+	)
+goto :EOF
+    
+:LOCATE_JAVA_OPTS
+  if "%JAVA_OPTS%" == "" (
+    set JAVA_OPTS=-Xms%JAVA_MIN_MEM% -Xmx%JAVA_MIN_MEM%
+  )
+  if "%MESHKEEPER_DEBUG%" == "" goto :END_MESHKEEPER_DEBUG
+  
+    if "%JAVA_DEBUG_OPTS%" == "" (
+      set JAVA_DEBUG_OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
+    )
+  
+    echo Enabling Java debug options: %JAVA_DEBUG_OPTS%
+    set JAVA_OPTS=%JAVA_DEBUG_OPTS% %JAVA_OPTS%
+  :END_MESHKEEPER_DEBUG  
+goto :EOF
+    
+:LOCATE_OPTS
+  set OPTS=-Dlog4j.configuration=file:%MESHKEEPER_HOME%\etc\log4j.properties
+  set OPTS=%OPTS% -Dmop.base=%MESHKEEPER_HOME%
+  set OPTS=%OPTS% -Dmop.online=false
+  set OPTS=%OPTS% %MESHKEEPER_OPTS%
+goto :EOF
+
+:LOCATE_CLASSPATH
+  set CLASSPATH=%MESHKEEPER_HOME%\repository\org\fusesource\meshkeeper\meshkeeper-api\${project.version}\meshkeeper-api-${project.version}.jar
+  set CLASSPATH=%CLASSPATH%;%MESHKEEPER_HOME%\repository\org\fusesource\mop\mop-core\${mop-version}\mop-core-${mop-version}.jar
+goto :EOF
+    
+:ERROR
+  echo Paused to catch any errors. Press any key to continue.
+  errorlvl 1    
+  pause
+goto :END
 
 :END
 endlocal
--- assembly/src/main/distribution/windows-text/bin/mk-server.bat
+++ assembly/src/main/distribution/windows-text/bin/mk-server.bat
@@ -1,38 +1,105 @@
-echo off
+@echo off
+REM ######################################################################################
+REM # Copyright (C) 2009 Progress Software, Inc. All rights reserved.                    #
+REM # http://fusesource.com                                                              #
+REM # ---------------------------------------------------------------------------------- #
+REM # The software in this package is published under the terms of the AGPL license      #
+REM # a copy of which has been included with this distribution in the license.txt file.  #
+REM ######################################################################################
+
 setlocal
-TITLE MeshKeeper Server
+TITLE MeshKeeper Control Server
 
-REM  runagent.bat
+REM =====================================================================
+REM User customization area.  You can set your defaults here.
+REM =====================================================================
 
-REM  ---------------------------------------------------------------------------
-REM  Usage:   runagent.bat
-REM  Purpose: See the meshkeeper documentation for the explanation
-REM  ---------------------------------------------------------------------------
-
-REM  -------- USER MUST SET THE FOLLOWING VARIABLES AFTER INSTALL --------------
-REM  JAVA_EXE should indicate the path to a 1.5 (or higher) java executable
-REM  ---------------------------------------------------------------------------
-
-REM Set this to a particular JAVA_EXE if you wish
 set JAVA_EXE=
+set JAVA_MIN_MEM=32m
+set JAVA_MAX_MEM=128m
+set MESHKEEPER_OPTS=
 
-REM Setup the Java Virtual Machine
+REM =====================================================================
+REM Execute the Control Server
+REM =====================================================================
 
-goto BEGIN
+call :SETUP_DEFAULTS
 
-:warn
-    echo runagent %*
+%JAVA_EXE% %JAVA_OPTS% %OPTS% -classpath %CLASSPATH% org.fusesource.meshkeeper.control.Main --directory "%MESHKEEPER_HOME%\data" %*
+
+if ERRORLEVEL 1 goto ERROR
+goto END
+
+REM =====================================================================
+REM Environment setup helper routines
+REM =====================================================================
+
+:SETUP_DEFAULTS
+  call :LOCATE_MESHKEEPER_HOME
+  call :LOCATE_MESHKEEPER_BASE
+  call :LOCATE_JAVA_EXE
+  call :LOCATE_JAVA_OPTS
+  call :LOCATE_OPTS
+  call :LOCATE_CLASSPATH
+goto :EOF
+
+:LOCATE_MESHKEEPER_HOME
+  cd %~dp0%
+  cd ..
+  set MESHKEEPER_HOME=%cd%
+  if not exist "%MESHKEEPER_HOME%" (
+    echo MESHKEEPER_HOME directory is not valid: %MESHKEEPER_HOME%
+    goto :ERROR
+  )
+goto :EOF
+
+:LOCATE_MESHKEEPER_BASE
+  if "%MESHKEEPER_BASE%" == "" (
+    set MESHKEEPER_BASE=%MESHKEEPER_HOME%
+  )
+goto :EOF
+
+:LOCATE_JAVA_EXE
+  if "%JAVA_EXE%" == "" (
+    set JAVA_EXE=%JAVA%
+  )
+	if "%JAVA_EXE%" == "" (
+    set JAVA_EXE=java
+	)
+goto :EOF
+    
+:LOCATE_JAVA_OPTS
+  if "%JAVA_OPTS%" == "" (
+    set JAVA_OPTS=-Xms%JAVA_MIN_MEM% -Xmx%JAVA_MIN_MEM%
+  )
+  if "%MESHKEEPER_DEBUG%" == "" goto :END_MESHKEEPER_DEBUG
+  
+    if "%JAVA_DEBUG_OPTS%" == "" (
+      set JAVA_DEBUG_OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
+    )
+  
+    echo Enabling Java debug options: %JAVA_DEBUG_OPTS%
+    set JAVA_OPTS=%JAVA_DEBUG_OPTS% %JAVA_OPTS%
+  :END_MESHKEEPER_DEBUG  
+goto :EOF
+    
+:LOCATE_OPTS
+  set OPTS=-Dlog4j.configuration=file:%MESHKEEPER_HOME%\etc\log4j.properties
+  set OPTS=%OPTS% -Dmop.base=%MESHKEEPER_HOME%
+  set OPTS=%OPTS% -Dmop.online=false
+  set OPTS=%OPTS% %MESHKEEPER_OPTS%
+goto :EOF
+
+:LOCATE_CLASSPATH
+  set CLASSPATH=%MESHKEEPER_HOME%\repository\org\fusesource\meshkeeper\meshkeeper-api\${project.version}\meshkeeper-api-${project.version}.jar
+  set CLASSPATH=%CLASSPATH%;%MESHKEEPER_HOME%\repository\org\fusesource\mop\mop-core\${mop-version}\mop-core-${mop-version}.jar
+goto :EOF
+    
+:ERROR
+  echo Paused to catch any errors. Press any key to continue.
+  errorlvl 1    
+  pause
 goto :END
 
-:BEGIN
-
-CALL setenv.bat
-if ERRORLEVEL 1 goto warn
-
-echo ------- Starting Agent -------
-%JAVA_EXE% %OPTS% -classpath %CLASSPATH% org.fusesource.meshkeeper.control.Main -dataDir "%MESHKEEPER_HOME%\data" %*
-
 :END
-echo Paused to catch any errors. Press any key to continue.
 endlocal
-pause
