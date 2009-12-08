acaf76a8be99b264bbb80adc34c1a2fedb2b650b
Hiram Chirino <hiram@hiramchirino.com>
2009-12-08 16:55:23 -0500

    Fixing header

--- meshkeeper-examples/example-b/src/test/java/RegistryTest.java
+++ meshkeeper-examples/example-b/src/test/java/RegistryTest.java
@@ -26,14 +26,6 @@ import org.fusesource.meshkeeper.MeshKeeperFactory;
 import org.fusesource.meshkeeper.MeshContainer.Hostable;
 import org.fusesource.meshkeeper.MeshContainer.MeshContainerContext;
 
-/**************************************************************************************
- * Copyright (C) 2009 Progress Software, Inc. All rights reserved.                    *
- * http://fusesource.com                                                              *
- * ---------------------------------------------------------------------------------- *
- * The software in this package is published under the terms of the AGPL license      *
- * a copy of which has been included with this distribution in the license.txt file.  *
- **************************************************************************************/
-
 /**
  * RegistryTest
  * <p>
